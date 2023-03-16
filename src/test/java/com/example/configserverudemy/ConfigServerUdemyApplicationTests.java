bfa63c61acdb28777eebf601abb3dc0cad1b5329
Vpp2000 <victor.ponce.p@uni.pe>
2023-03-16 17:05:52 -0500

    initializing config server

---
+++ src/test/java/com/example/configserverudemy/ConfigServerUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configserverudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerUdemyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
